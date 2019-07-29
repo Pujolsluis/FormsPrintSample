93ec16975cdab3944d04f60b69c48992bce49c1d
Luis Pujols <luispujols@hotmail.com>
2019-07-28 21:43:04 -0400

    Added base project structure, and Prism

---
+++ FormsPrintSample/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FormsPrintSample.ViewModels;
+using Prism;
+using Prism.Ioc;
+using Prism.Unity;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FormsPrintSample
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer platformInitializer) : base(platformInitializer)
+        {
+        }
+
+        protected override async void OnInitialized()
+        {
+            await NavigationService.NavigateAsync("MainPage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+        }
+    }
+}
---
+++ FormsPrintSample/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace FormsPrintSample.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public string Test { get; set; }
+        public MainPageViewModel()
+        {
+            Test = "Hello";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
